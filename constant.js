f5eb9086dd78baef8c3d13354ecc1af0733bf5ab
Ilham Muhammad Ramdhan Gunawan <ilhammrg@gmail.com>
2022-07-29 08:30:30 +0700

    create constant

---
+++ constant.js
@@ -0,0 +1,9 @@
+/*
+  WARNING!
+  Please do not mutate any values in this module!
+*/
+
+module.exports = {
+  sortedNumbers: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  unsortedNumbers: [10, 5, 3, 8, 2, 6, 4, 7, 9, 1],
+};
